1e698d0bbb65793e3d2a3beb6db14e5ddf58b745
B3br0s <bogdanlysergey@gmail.com>
2024-08-17 22:12:46 +0300

    ну я хз,удалил строку

--- addons/homigrad/lua/homigrad_scr/game/tier_1/fake/gib/init_sv.lua
+++ addons/homigrad/lua/homigrad_scr/game/tier_1/fake/gib/init_sv.lua
@@ -198,7 +198,6 @@ function Gib_Input(rag,bone,dmgInfo,player)
 	end
 	--ахуенно когда взрываеца от прикосновения да?
 	--да
-	--КАКОЙ ПРЕКРАСНЫЙ ДЕНЬ,ЕСЛИ ТЫ ОЛЕНЬ
 	if dmgInfo:GetDamage() >= 300 and dmgInfo:IsDamageType(DMG_CRUSH+DMG_BLAST+DMG_VEHICLE+DMG_FALL) or rag:GetVelocity():Length() > 700 and dmgInfo:IsDamageType(DMG_CRUSH+DMG_BLAST+DMG_VEHICLE+DMG_FALL) then
 			dmgInfo:ScaleDamage(5000)
 			sound.Emit(rag,"player/headshot" .. math.random(1,2) .. ".wav")
